e97d514abe6c29de628b74486d5a0a000eb70fab
Qiusheng Wu <giswqs@gmail.com>
2021-10-07 23:58:21 -0400

    Updated README

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # geemap-apps
 
-A multi-page interactive web app created using streamlit and geemap.
+## Introduction
+
+This repo demostrates how to build a multi-page [Earth Engine](https://earthengine.google.com) App using [streamlit](https://streamlit.io) and [geemap](https://geemap.org). You can deploy the app on various cloud platforms, such as [share.streamlit.io](https://share.streamlit.io) or [Heroku](https://heroku.com). Make sure you set `EARTHENGINE_TOKEN='your-token'` as an environment variable (secret) on the cloud platform.
+
+- **Web App:** <https://gishub.org/geemap-apps>
+- **Github:** <https://github.com/giswqs/geemap-apps>
+
+### Where to find your Earth Engine token?
+
+- **Windows:** `C:/Users/USERNAME/.config/earthengine/credentials`
+- **Linux:** `/home/USERNAME/.config/earthengine/credentials`
+- **macOS:** `/Users/USERNAME/.config/earthengine/credentials`
 
 ## Credits
 
--- apps/home.py
+++ apps/home.py
@@ -7,11 +7,22 @@ def app():
     st.header("Introduction")
     st.markdown(
         """
-    This site (<https://gishub.org/geemap-apps>) demostrates how to build a multi-page interactive web app using [streamlit](https://streamlit.io), [geemap](https://geemap.org), and [Google Earth Engine](https://earthengine.google.com).
-    You can deploy the app on various platforms, such as [share.streamlit.io](https://share.streamlit.io) or [Heroku](https://heroku.com).
-    Make sure you set `EARTHENGINE_TOKEN='your-token'` as an environment variable.
+    This site demostrates how to build a multi-page [Earth Engine](https://earthengine.google.com) App using [streamlit](https://streamlit.io) and [geemap](https://geemap.org).
+    You can deploy the app on various cloud platforms, such as [share.streamlit.io](https://share.streamlit.io) or [Heroku](https://heroku.com).
+    Make sure you set `EARTHENGINE_TOKEN='your-token'` as an environment variable (secret) on the cloud platform.
+    - **Web App:** <https://gishub.org/geemap-apps>
+    - **Github:** <https://github.com/giswqs/geemap-apps>
     """
     )
+
+    with st.expander("Where to find your Earth Engine token?"):
+        st.markdown(
+            """
+            - **Windows:** `C:/Users/USERNAME/.config/earthengine/credentials`
+            - **Linux:** `/home/USERNAME/.config/earthengine/credentials`
+            - **macOS:** `/Users/USERNAME/.config/earthengine/credentials`
+            """
+        )
 
     st.header("Example")
 
