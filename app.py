02e636ff99cedbff7a6cdc48da06eb009b8ef18a
Qiusheng Wu <giswqs@gmail.com>
2021-11-18 16:25:39 -0500

    Updated app demo

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, basemaps, customize, datasets, opacity, nlcd
+from apps import home, basemaps, customize, datasets, opacity, nlcd_demo
 
 st.set_page_config(layout="wide")
 
@@ -14,7 +14,7 @@ apps.add_app("Customize the default map", customize.app)
 apps.add_app("Change basemaps", basemaps.app)
 apps.add_app("Change opacity", opacity.app)
 apps.add_app("Search datasets", datasets.app)
-apps.add_app("NLCD", nlcd.app)
+apps.add_app("NLCD Demo", nlcd_demo.app)
 
 # The main app
 apps.run()
