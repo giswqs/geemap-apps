f4ff28d2ebb86523a846e3c4d7ed81f0e2601413
Qiusheng Wu <giswqs@gmail.com>
2023-07-15 21:34:40 -0400

    Update repo

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ## Introduction
 
-This repo demostrates how to build a multi-page [Earth Engine](https://earthengine.google.com) App using [streamlit](https://streamlit.io) and [geemap](https://geemap.org). You can deploy the app on various cloud platforms, such as [share.streamlit.io](https://share.streamlit.io) or [Heroku](https://heroku.com). Make sure you set `EARTHENGINE_TOKEN='your-token'` as an environment variable (secret) on the cloud platform.
+This repo demostrates how to build a multi-page [Earth Engine](https://earthengine.google.com) App using [streamlit](https://streamlit.io) and [geemap](https://geemap.org). You can deploy the app on various cloud platforms, such as [share.streamlit.io](https://share.streamlit.io). Make sure you set `EARTHENGINE_TOKEN='your-token'` as an environment variable (secret) on the cloud platform.
 
-- **Web App:** <https://gishub.org/geemap-apps>
+- **Web App:** <https://geemap.streamlit.app>
 - **Github:** <https://github.com/giswqs/geemap-apps>
 
 ### Where to find your Earth Engine token?
@@ -12,7 +12,3 @@ This repo demostrates how to build a multi-page [Earth Engine](https://earthengi
 - **Windows:** `C:/Users/USERNAME/.config/earthengine/credentials`
 - **Linux:** `/home/USERNAME/.config/earthengine/credentials`
 - **macOS:** `/Users/USERNAME/.config/earthengine/credentials`
-
-## Credits
-
-This app is created using the template from [upraneelnihar/streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps).
--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import streamlit as st
-import leafmap.foliumap as leafmap
+import geemap.foliumap as geemap
 
 st.set_page_config(layout="wide")
 
 # Customize the sidebar
 markdown = """
-Web App URL: <https://geotemplate.streamlit.app>
-GitHub Repository: <https://github.com/giswqs/geemap-apps>
+Web App URL: <https://geemap.streamlit.app>
 """
 
 st.sidebar.title("About")
@@ -15,26 +14,25 @@ logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
 
 # Customize page title
-st.title("Streamlit for Geospatial Applications")
+st.title("Earth Engine Web App")
 
 st.markdown(
     """
-    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template).
+    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [geemap](https://geemap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/giswqs/geemap-apps).
     """
 )
 
 st.header("Instructions")
 
 markdown = """
-1. For the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template) or [use it as a template](https://github.com/giswqs/streamlit-multipage-template/generate) for your own project.
+1. For the [GitHub repository](https://github.com/giswqs/geemap-apps) or [use it as a template](https://github.com/new?template_name=geemap-apps&template_owner=giswqs) for your own project.
 2. Customize the sidebar by changing the sidebar text and logo in each Python files.
 3. Find your favorite emoji from https://emojipedia.org.
 4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
-
 """
 
 st.markdown(markdown)
 
-m = leafmap.Map(minimap_control=True)
+m = geemap.Map()
 m.add_basemap("OpenTopoMap")
 m.to_streamlit(height=500)
